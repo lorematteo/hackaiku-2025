3309fa06ea6dd0078ffa07d6bb041b6f11a68a63
lorematteo <matteo.lore@dataiku.com>
2025-04-29 15:58:45 +0200

    evol(right-panel): panel title component

---
+++ src/features/right-panel/index.tsx
@@ -0,0 +1,11 @@
+import PanelTitle from './components/title';
+
+const RightPanel: React.FC = () => {
+  return (
+    <div className="flex flex-col bg-white w-[744px] border-l border-base-300">
+      <PanelTitle />
+    </div>
+  );
+};
+
+export default RightPanel;
