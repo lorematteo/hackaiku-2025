51de566cdf45a58042370c4f471c4730439b1ce2
lorematteo <matteo.lore@dataiku.com>
2025-04-30 12:52:05 +0200

    feat(canva): adding agent node

---
+++ src/features/canva/nodes/agent.tsx
@@ -0,0 +1,31 @@
+import { Handle, Node, NodeProps, Position } from '@xyflow/react';
+
+import NodeIcon from '@/components/node-icons';
+import { NodeType } from '@/const/nodes';
+
+type AgentNodeType = Node<NodeType, 'agent'>;
+
+const AgentNode: React.FC<NodeProps<AgentNodeType>> = ({ data, isConnectable }) => {
+  return (
+    <div className="bg-white border rounded-full px-3 py-3 border-gray-300">
+      <div className="flex flex-row gap-2 items-center justify-center">
+        <NodeIcon name={data.icon} />
+        <p>{data.name}</p>
+      </div>
+      <Handle
+        type="source"
+        className="!bg-gray-400 !size-2 rounded-full"
+        position={Position.Left}
+        isConnectable={isConnectable}
+      />
+      <Handle
+        type="source"
+        className="!bg-gray-400 !size-2 rounded-full"
+        position={Position.Right}
+        isConnectable={isConnectable}
+      />
+    </div>
+  );
+};
+
+export default AgentNode;
